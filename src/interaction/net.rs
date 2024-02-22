37b7dbff223d4635a9b8aebddb826f735fdf72b2
Eduardo Sandalo Porto <edu@sandalo.dev>
2024-02-22 11:34:14 -0300

    Small refactoring

--- src/interaction/net.rs
+++ src/interaction/net.rs
@@ -1,143 +1,162 @@
-use crate::interaction::graph::Cell;
+use std::collections::HashMap;
 
-use super::graph::{CellKey, CellType, InteractionGraph};
+use slab::Slab;
 
-/// Interaction net
-pub struct Net {
-    graph: InteractionGraph,
+/// Key for Slabs
+type Key = usize;
+
+/// Represents a port in the Interaction Net
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+pub struct PortKey(Key);
+
+/// Represents a cell in the Interaction Net
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+pub struct CellKey(Key);
+
+/// Interaction Combinator
+#[derive(Clone, Copy)]
+pub struct Cell {
+    pub(crate) primary: PortKey,
+    pub(crate) cell_type: CellType,
 }
 
-impl Net {
-    pub fn normalize(&mut self) {
-        while self.perform_any_interaction() {}
+/// Types of Interaction Combinators
+#[derive(Clone, Copy)]
+pub enum CellType {
+    /// Eraser
+    Era,
+    /// Constructor
+    Con(PortKey, PortKey),
+    /// Duplicator
+    Dup(PortKey, PortKey),
+}
+
+/// Ports can be free or part of a cell. They are connected to
+/// each other by wires in the interaction net.
+#[derive(Clone, Copy)]
+pub struct Port {
+    pub(crate) cell: Option<CellKey>,
+}
+
+/// Interaction Net
+///
+/// Contains interaction combinators, the ports that connect them, and
+/// the wires between the ports.
+pub struct InteractionNet {
+    ports: Slab<Port>,
+    cells: Slab<Cell>,
+    wires: HashMap<PortKey, PortKey>,
+}
+
+impl InteractionNet {
+    pub fn new() -> Self {
+        Self {
+            ports: Slab::new(),
+            cells: Slab::new(),
+            wires: HashMap::new(),
+        }
     }
 
-    pub fn perform_any_interaction(&mut self) -> bool {
-        let mut connected_cells = self
-            .graph
-            .get_cells()
-            .map(|(cell_key, _)| {
-                // Match cell with its primary connection port
-                let cell = self.graph.get_cell(cell_key);
-                (cell_key, self.graph.get_port(cell.primary).cell)
-            })
-            .filter_map(|(cell_key1, connected_cell)| {
-                // Only include cells with another cell connected to their primary port
-                connected_cell.map(|cell_key2| (cell_key1, cell_key2))
-            });
+    pub fn create_port(&mut self) -> PortKey {
+        let port = Port { cell: None };
+        PortKey(self.ports.insert(port))
+    }
 
-        let interactable = connected_cells.find(|(c1, c2)| self.can_interact(*c1, *c2));
-
-        if let Some((cell_key1, cell_key2)) = interactable {
-            drop(connected_cells);
-            self.interact(cell_key1, cell_key2);
-            return true;
+    pub fn remove_port(&mut self, port_key: PortKey) {
+        if self.ports[port_key.0].cell.is_some() || self.wires.contains_key(&port_key) {
+            panic!("can only remove unconnected free ports");
         }
 
-        false
+        self.ports.remove(port_key.0);
     }
 
-    pub fn can_interact(&self, cell_key1: CellKey, cell_key2: CellKey) -> bool {
-        let cell1 = *self.graph.get_cell(cell_key1);
-        let cell2 = *self.graph.get_cell(cell_key2);
-
-        self.graph.ports_connected(cell1.primary, cell2.primary)
-    }
-
-    pub fn try_interact(&mut self, cell_key1: CellKey, cell_key2: CellKey) -> bool {
-        if self.can_interact(cell_key1, cell_key2) {
-            self.interact(cell_key1, cell_key2);
-        }
-        true
-    }
-
-    fn interact(&mut self, cell_key1: CellKey, cell_key2: CellKey) {
-        let cell1 = *self.graph.get_cell(cell_key1);
-        let cell2 = *self.graph.get_cell(cell_key2);
-
-        // if !self.graph.ports_connected(cell1.primary, cell2.primary) {
-        //     return;
-        // }
-
-        self.graph.disconnect_ports(cell1.primary, cell2.primary);
-        let primary1 = cell1.primary;
-        let primary2 = cell2.primary;
-
-        use CellType::*;
-        match (cell1.cell_type, cell2.cell_type) {
-            (Era, Era) => {
-                self.graph.remove_cell(cell_key1);
-                self.graph.remove_cell(cell_key2);
-            }
-
-            (Era, Con(left, right))
-            | (Con(left, right), Era)
-            | (Era, Dup(left, right))
-            | (Dup(left, right), Era) => {
-                self.graph.remove_cell(cell_key1);
-                self.graph.remove_cell(cell_key2);
-
-                let port1 = self.graph.create_port();
-                self.graph.connect_ports(port1, left);
-                self.graph.insert_cell(Cell {
-                    primary: port1,
-                    cell_type: Era,
-                });
-
-                let port2 = self.graph.create_port();
-                self.graph.connect_ports(port2, right);
-                self.graph.insert_cell(Cell {
-                    primary: port2,
-                    cell_type: Era,
-                });
-            }
-
-            (Con(left1, right1), Con(left2, right2)) | (Dup(left1, right1), Dup(left2, right2)) => {
-                self.graph.remove_cell(cell_key1);
-                self.graph.remove_cell(cell_key2);
-
-                self.graph.connect_ports(left1, left2);
-                self.graph.connect_ports(right1, right2);
-            }
-
-            (Con(left1, right1), Dup(left2, right2)) | (Dup(left2, right2), Con(left1, right1)) => {
-                self.graph.remove_cell(cell_key1);
-                self.graph.remove_cell(cell_key2);
-
-                let (upper_dup_left, upper_dup_right) =
-                    (self.graph.create_port(), self.graph.create_port());
-                let (upper_con_left, upper_con_right) =
-                    (self.graph.create_port(), self.graph.create_port());
-                let (lower_dup_left, lower_dup_right) =
-                    (self.graph.create_port(), self.graph.create_port());
-                let (lower_con_left, lower_con_right) =
-                    (self.graph.create_port(), self.graph.create_port());
-
-                let _upper_dup = self.graph.insert_cell(Cell {
-                    primary: left1,
-                    cell_type: Dup(upper_dup_left, upper_dup_right),
-                });
-                let _upper_con = self.graph.insert_cell(Cell {
-                    primary: right2,
-                    cell_type: Con(upper_con_left, upper_con_right),
-                });
-                let _lower_dup = self.graph.insert_cell(Cell {
-                    primary: right1,
-                    cell_type: Dup(lower_dup_left, lower_dup_right),
-                });
-                let _lower_con = self.graph.insert_cell(Cell {
-                    primary: left2,
-                    cell_type: Con(lower_con_left, lower_con_right),
-                });
-
-                self.graph.connect_ports(upper_dup_right, upper_con_left);
-                self.graph.connect_ports(upper_dup_left, lower_con_left);
-                self.graph.connect_ports(lower_dup_right, upper_con_right);
-                self.graph.connect_ports(lower_dup_left, lower_con_right);
-            }
+    pub fn connect_ports(&mut self, p1: PortKey, p2: PortKey) {
+        if p1 == p2 {
+            panic!("cannot connect port to itself");
         }
 
-        self.graph.remove_port(primary1);
-        self.graph.remove_port(primary2);
+        self.wires.insert(p1, p2);
+        self.wires.insert(p2, p1);
+    }
+
+    pub fn disconnect_ports(&mut self, p1: PortKey, p2: PortKey) {
+        if self.wires.get(&p1) == Some(&p2) {
+            self.wires.remove(&p1);
+            self.wires.remove(&p2);
+        }
+    }
+
+    pub fn get_port(&self, port_key: PortKey) -> &Port {
+        &self.ports[port_key.0]
+    }
+
+    pub fn ports_connected(&self, port_key1: PortKey, port_key2: PortKey) -> bool {
+        self.wires.get(&port_key1).map(|port| *port == port_key2) == Some(true)
+    }
+
+    pub fn insert_cell(&mut self, cell: Cell) -> CellKey {
+        let cell_key = CellKey(self.cells.insert(cell));
+        self.ports[cell.primary.0].cell = Some(cell_key);
+        match cell.cell_type {
+            CellType::Con(p1, p2) => {
+                self.ports[p1.0].cell = Some(cell_key);
+                self.ports[p2.0].cell = Some(cell_key);
+            }
+            CellType::Dup(p1, p2) => {
+                self.ports[p1.0].cell = Some(cell_key);
+                self.ports[p2.0].cell = Some(cell_key);
+            }
+            CellType::Era => (),
+        }
+
+        cell_key
+    }
+
+    pub fn remove_cell(&mut self, cell_key: CellKey) {
+        let cell = self.cells[cell_key.0];
+        self.ports[cell.primary.0].cell = None;
+        match cell.cell_type {
+            CellType::Con(p1, p2) => {
+                self.ports[p1.0].cell = None;
+                self.ports[p2.0].cell = None;
+            }
+            CellType::Dup(p1, p2) => {
+                self.ports[p1.0].cell = None;
+                self.ports[p2.0].cell = None;
+            }
+            CellType::Era => (),
+        }
+
+        self.cells.remove(cell_key.0);
+    }
+
+    pub fn get_cell(&self, cell_key: CellKey) -> &Cell {
+        &self.cells[cell_key.0]
+    }
+
+    pub fn get_cells(&self) -> impl Iterator<Item = (CellKey, &Cell)> {
+        self.cells.iter().map(|(key, cell)| (CellKey(key), cell))
+    }
+}
+
+impl Default for InteractionNet {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use slab::Slab;
+
+    #[test]
+    fn test() {
+        let mut slab = Slab::new();
+        slab.insert(1);
+        slab.insert(2);
+        slab.insert(1);
+
+        println!("{slab:?}");
+        println!("{:?}", slab.iter().collect::<Vec<_>>());
     }
 }
